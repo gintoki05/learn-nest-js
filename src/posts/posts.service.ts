5d16b8746179120026bdbfe4789e3945759c0d7f
mustechs <musitechnicalsolution@gmail.com>
2021-09-09 22:28:05 +0700

    first commit

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,31 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { Post, PostDocument } from './schemas/posts.schema';
+
+@Injectable()
+export class PostsService {
+  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+
+  create(createPostDto: CreatePostDto) {
+    return 'This action adds a new post';
+  }
+
+  async findAll(): Promise<Post[]> {
+    return this.postModel.find().exec();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} post`;
+  }
+
+  update(id: number, updatePostDto: UpdatePostDto) {
+    return `This action updates a #${id} post`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} post`;
+  }
+}
